825e2efe25409e684e8ff4690483171cb19433ad
QuantumParrot <ataraxia528491@gmail.com>
2024-01-17 16:39:00 +0800

    initial commit

---
+++ src/components/Card.vue
@@ -0,0 +1,24 @@
+<template>
+
+    <p>這是一個卡片元件</p>
+    <p>{{ text }}</p>
+
+</template>
+
+<script>
+
+export default {
+
+    data() {
+
+        return {
+
+            text: '這是卡片裡的文字'
+
+        }
+
+    },
+
+}
+
+</script>
